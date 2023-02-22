c567f0a5f8835cff23c7539b5951e15eb650db06
Diego Vanegas <diegoalejandrovanegas97@gmail.com>
2023-02-22 17:01:26 -0500

    Llamado a los componentes dentro de React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
-function App() {
-  return <h1 className="main-title">Aplicación TODO's</h1>;
+import React from "react";
+
+import { TodoCounter } from "./TodoCounter.js";
+import { TodoSearch } from "./TodoSearch.js";
+import { TodoList } from "./TodoList.js";
+import { TodoItem } from "./TodoItem.js";
+import { CreateTodoButton } from "./CreateTodoButton.js";
+
+const listaTodos = [
+  { text: 'Cumpleaños juan', completed: false },
+  { text: 'Compras verduras', completed: false },
+  { text: 'intro curso reactJs', completed: false }
+]
+
+// componente
+function App(props) {
+  // elementos JSX
+  return (
+    <React.Fragment>
+       <TodoCounter/>
+
+      <TodoSearch/>
+
+      <TodoList>
+        {listaTodos.map( todo => (
+
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+          />
+
+        ))}
+
+      </TodoList>
+
+      <CreateTodoButton/>
+    </React.Fragment>
+  );
 }
 
 export { App };
