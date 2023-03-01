5aaaf7fe61073e876ed9d2cf5b015c00c6664ae1
Diego Vanegas <diegoalejandrovanegas97@gmail.com>
2023-02-28 19:37:10 -0500

    manejo de eventos para cambiar el estado de los TODO's

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,13 +3,30 @@ import "./css/TodoItem.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function TodoItem(props) {
+
+    const onClick = () => {
+        alert('Aqui el check se marcaria verde✅');
+    }
+
+    const onDelete = () => {
+        alert('Se borro la nota✖️')
+    }
+
     return(
         <li className="TodoItem">
-            <i className={`bi bi-check-lg ${props.completed && 'TodoItem-check'}`}></i>
+            <i
+                className={`bi bi-check-lg ${props.completed && 'TodoItem-check'}`}
+                onClick={onClick}
+            >
+            </i>
             <p className={`${props.completed && 'texto-marcado'}`}>
                 {props.text}
             </p>
-            <i className='bi bi-x-lg'></i>
+            <i
+                className='bi bi-x-lg'
+                onClick={onDelete}
+            >
+            </i>
         </li>
     );
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,10 +3,19 @@ import "./css/TodoSearch.css";
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 function TodoSearch() {
+
+    const searchValueChange = (event) => {
+        console.log(event.target.value);
+    }
+
     return(
         <div className="TodoSearch-container">
             <i className="bi bi-search"></i>
-            <input className="TodoSearch" placeholder="Buscar..."></input>
+            <input
+                className="TodoSearch"
+                placeholder="Buscar..."
+                onChange={searchValueChange}
+            ></input>
         </div>
     );
 }
